9fae45f7b2efd238367109118fb66f832ceeea13
ivalencius <ilanvalencius@gmail.com>
2020-11-17 21:35:38 -0500

    v1: digits not in the thousands

---
+++ hw10_q3_Euclid.py
@@ -0,0 +1,21 @@
+import random
+import sys
+sys.setrecursionlimit(2500)
+
+def extended_Euclid(n1,n2):
+    if n2==0:
+        return (1,0,n1)
+    else:
+        (q,r) = divmod(n1,n2)
+        (a,b,d) = extended_Euclid(n2,r) 
+    return (b, a-b*q, d)   
+
+def main():
+    random_num1 = random.getrandbits(1000)
+    random_num2 = random.getrandbits(1000)
+    (a, b, gcd) = extended_Euclid(random_num1, random_num2)
+    print("Number 1 = \n%d" %(random_num1))
+    print("Number 2 = \n%d"%(random_num2))
+    print("a=\n%d\nb=\n%d\ngcd=%d"%(a,b,gcd))
+    
+main()
