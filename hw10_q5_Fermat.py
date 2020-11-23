7c24d6ca740c3756c89d06073d6e7cfd3abd9073
ivalencius <ilanvalencius@gmail.com>
2020-11-22 21:17:06 -0500

    added fermatPrime fxn; need to up digits

--- hw10_q5_Fermat.py
+++ hw10_q5_Fermat.py
@@ -1,5 +1,7 @@
 # Ilan Valencius
 import random
+import sys
+sys.setrecursionlimit(2500)
 
 def fermatTest(n,ntrials):
     if (ntrials == 0):
@@ -9,7 +11,17 @@ def fermatTest(n,ntrials):
         return False
     else:
         return fermatTest(n,ntrials-1)
-    
+
+def fermatPrime(max, ntrials):
+    rand_num = random.randint(0, max)
+    while(not fermatTest(rand_num,ntrials)):
+        rand_num = random.randrange(max)
+    return rand_num
+
 def main():
-    random_num1 = random.getrandbits(6700)
-    random_num2 = random.getrandbits(6700)
+    max = 1000000
+    # set ntrials = 10 for P(false positive) < 0.1%
+    ntrials = 10
+    print("%d\n\n is a probably prime number between 0 and \n\n%d"%(fermatPrime(max,ntrials),max))
+    
+main()
