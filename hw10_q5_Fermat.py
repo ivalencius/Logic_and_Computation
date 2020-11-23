5fcd2b5d060b3b002ceb6b00f4e5208b361753ae
ivalencius <ilanvalencius@gmail.com>
2020-11-22 22:08:46 -0500

    can run on numbers up to 10E500

--- hw10_q5_Fermat.py
+++ hw10_q5_Fermat.py
@@ -6,7 +6,7 @@ sys.setrecursionlimit(2500)
 def fermatTest(n,ntrials):
     if (ntrials == 0):
         return True
-    a = random.randint(2,n)
+    a = random.randrange(1,n-1)
     if (pow(a,n-1,n) != 1): 
         return False
     else:
@@ -19,7 +19,7 @@ def fermatPrime(max, ntrials):
     return rand_num
 
 def main():
-    max = 1000000
+    max = random.randint(0,10**500)
     # set ntrials = 10 for P(false positive) < 0.1%
     ntrials = 10
     print("%d\n\n is a probably prime number between 0 and \n\n%d"%(fermatPrime(max,ntrials),max))
